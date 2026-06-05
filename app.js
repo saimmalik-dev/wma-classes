dd9cf4a8f48c6391dc6edf31c2a266d5abda2253
saimmalik.dev <saimalik.dev@gmail.com>
2026-06-05 14:02:23 +0500

    js class1

---
+++ app.js
@@ -0,0 +1,42 @@
+// // console.log("hello world")
+
+// // // alert("hello world")
+
+// // var stdName = "Fahad"
+
+// var name = "NO nAME";
+// // console.log(name) //Asad
+// name = "Ali"
+// console.log(name) // output
+// name = "Asad"
+// console.log(name) // output
+// alert(name)
+
+// // name = "New Name"
+// // console.log(name) // output
+// // console.log(name)
+
+
+var weight = 150;
+var secondWeight = 200;
+var newWeight = weight + secondWeight;
+console.log(newWeight);
+
+
+var originalNum = "23";
+var newNum = originalNum + 7;
+console.log(newNum)
+
+
+var studentName = "";
+var stdName = "";
+
+
+
+var floatNum = 80.24;
+var floatNum2 = 20.12;
+var sum = floatNum + floatNum2;
+console.log(sum)
+
+
+
