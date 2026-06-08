b2d03b8a24a52ec2a7068b52fb402a18438a8e15
saimmalik.dev <saimalik.dev@gmail.com>
2026-06-08 16:09:18 +0500

    jsclass 3

--- app.js
+++ app.js
@@ -1,134 +1,60 @@
-// // // console.log("hello world")
+// var num = 0;
+// num = num + 1; // increment operator
+// ++num; // num = num+1;
+// console.log(num);
 
-// // // // alert("hello world")
-
-// // // var stdName = "Fahad"
-
-// // var name = "NO nAME";
-// // console.log(name) //Asad
-// // name = "Ali"
-// // console.log(name) // output
-// // name = "Asad"
-// // console.log(name) // output
-// // alert(name)
-
-// // // name = "New Name"
-// // // console.log(name) // output
-// // // console.log(name)
-
-
-// // var weight = 150;
-// // var secondWeight = 200;
-// // var newWeight = weight + secondWeight;
-// // console.log(newWeight);
-
-
-// // var originalNum = "23";
-// // var newNum = originalNum + 7;
-// // console.log(newNum)
-
-
-// var studentName = "";
-// var stdName = "";
-
-
-// // var variable_name
+// // ++num; // pre-increment operator
+// // --num; // pre-decrement operator
 
 
 
 
-// // var variable_name = value
-// var floatNum = 255;
-// var floatNum2 = 5;
-// var float_num_2 = 5;
-// var FloatNum2 = 5;
-// var sum = floatNum + floatNum2;
-// console.log(sum)
+// // num++; post-increment operator 
+
+// // num = num - 1; // decrement operator
+
+
+// // var message = "Thanks for your input!";
+// // var newMeessage = "Thanks" + " User";
+// // console.log(newMeessage)
+
+// var name = "Asad";
+// alert("Hello " + name + ", Welcome to JavaScript!");
 
 
 
-// // var num1 = 2;
-// // var multiply = num1 * 2;
+// var sum = "23" + 7;
+// console.log(sum);
+
+
+// var name = prompt("What is your name?", "John");
+// console.log(name);
+
+// alert("Hello " + name + " Welcome")
+
+var num1 = prompt("Enter num 1?");
+var num2 = prompt("Enter num 2 ?");
+console.log(typeof num1, typeof num2);
+console.log(num1, num2);
+var sum = parseInt(num1) + parseInt(num2);
+alert("The sum is " + sum);
 
 
 
+var num1 = 3;
+var num2 = 4;
+console.log(num1 != num2);
 
-// // var num2 = prompt("Enter a number 2");
-// // var multiply2 = num * num2;
-
-
-// // var division = num / num2;
-// // console.log(division)
-
-
-// // var remainder = num % num2;
-// // console.log(remainder)
-
-
-
-// // var num = prompt("Enter a number")
-
-// var num = 5;
-// console.log(num)
-
-// num = num + 1;
-// num++
-
-// console.log(num)
-
-// num = num - 1
-// num--
-// console.log(num)
+// Comparision Operators
+// == // equality operator
+// != // not equal operator
+// < // less than operator
+// > // greater than operator
+// <= // less than or equal to operator
+// >= // greater than or equal to operator
 
 
-
-// // n++
-// // first + 1 then assign the value to n
-// // ++n
-// // first assign the value to n then add 1 to n
-
-// // var num = 1;
-// // var newNum = ++num;
-// // num = num + 1;
-// // var newNum = num;
-
-
-// // // var newNum = num++;
-// // var newNum = num;
-// // num = num + 1
-
-
-
-// // console.log("num", num)
-// // console.log("newNum", newNum)
-
-
-
-var totalCost = 1 + 3 * 4;
-
-console.log(totalCost)
-
-
-var obtainedMarks = 81;
-var totalMarks = 100;
-
-var avg = (2 + 3 + 4 + 5 + 6) / 5;
-
-console.log(avg + " avg")
-
-
-var num = "23" + 7;
-console.log(num)
-
-
-
-var userName = prompt("Enter user name")
-alert("Hello " + userName)
-
-
-
-
-var floatNum = 80.24;
-var floatNum2 = 20.12;
-var sum = floatNum + floatNum2;
-console.log(sum)
+// Logical Operators
+// && // logical AND operator
+// || // logical OR operator
+// ! // logical NOT operator
