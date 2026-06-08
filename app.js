2a1ca9d93c2f3a2c7dee8807ff77fed7a0765cee
saimmalik.dev <saimalik.dev@gmail.com>
2026-06-08 14:10:26 +0500

    js class 2

--- app.js
+++ app.js
@@ -1,35 +1,130 @@
-// // console.log("hello world")
+// // // console.log("hello world")
 
-// // // alert("hello world")
+// // // // alert("hello world")
 
-// // var stdName = "Fahad"
+// // // var stdName = "Fahad"
 
-// var name = "NO nAME";
+// // var name = "NO nAME";
 // // console.log(name) //Asad
-// name = "Ali"
-// console.log(name) // output
-// name = "Asad"
-// console.log(name) // output
-// alert(name)
-
-// // name = "New Name"
+// // name = "Ali"
 // // console.log(name) // output
-// // console.log(name)
+// // name = "Asad"
+// // console.log(name) // output
+// // alert(name)
+
+// // // name = "New Name"
+// // // console.log(name) // output
+// // // console.log(name)
 
 
-var weight = 150;
-var secondWeight = 200;
-var newWeight = weight + secondWeight;
-console.log(newWeight);
+// // var weight = 150;
+// // var secondWeight = 200;
+// // var newWeight = weight + secondWeight;
+// // console.log(newWeight);
 
 
-var originalNum = "23";
-var newNum = originalNum + 7;
-console.log(newNum)
+// // var originalNum = "23";
+// // var newNum = originalNum + 7;
+// // console.log(newNum)
 
 
-var studentName = "";
-var stdName = "";
+// var studentName = "";
+// var stdName = "";
+
+
+// // var variable_name
+
+
+
+
+// // var variable_name = value
+// var floatNum = 255;
+// var floatNum2 = 5;
+// var float_num_2 = 5;
+// var FloatNum2 = 5;
+// var sum = floatNum + floatNum2;
+// console.log(sum)
+
+
+
+// // var num1 = 2;
+// // var multiply = num1 * 2;
+
+
+
+
+// // var num2 = prompt("Enter a number 2");
+// // var multiply2 = num * num2;
+
+
+// // var division = num / num2;
+// // console.log(division)
+
+
+// // var remainder = num % num2;
+// // console.log(remainder)
+
+
+
+// // var num = prompt("Enter a number")
+
+// var num = 5;
+// console.log(num)
+
+// num = num + 1;
+// num++
+
+// console.log(num)
+
+// num = num - 1
+// num--
+// console.log(num)
+
+
+
+// // n++
+// // first + 1 then assign the value to n
+// // ++n
+// // first assign the value to n then add 1 to n
+
+// // var num = 1;
+// // var newNum = ++num;
+// // num = num + 1;
+// // var newNum = num;
+
+
+// // // var newNum = num++;
+// // var newNum = num;
+// // num = num + 1
+
+
+
+// // console.log("num", num)
+// // console.log("newNum", newNum)
+
+
+
+var totalCost = 1 + 3 * 4;
+
+console.log(totalCost)
+
+
+var obtainedMarks = 81;
+var totalMarks = 100;
+
+var avg = (2 + 3 + 4 + 5 + 6) / 5;
+
+console.log(avg + " avg")
+
+
+var num = "23" + 7;
+console.log(num)
+
+
+
+var userName = prompt("Enter user name")
+alert("Hello " + userName)
+
 
 
 
@@ -37,6 +132,3 @@ var floatNum = 80.24;
 var floatNum2 = 20.12;
 var sum = floatNum + floatNum2;
 console.log(sum)
-
-
-
