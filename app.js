c08475e351d6d1b5808bbf80aff163493ed9e7ae
saimmalik.dev <saimalik.dev@gmail.com>
2026-06-10 16:05:36 +0500

    js-class-4 if else statement

--- app.js
+++ app.js
@@ -1,51 +1,13 @@
-// var num = 0;
-// num = num + 1; // increment operator
-// ++num; // num = num+1;
-// console.log(num);
-
-// // ++num; // pre-increment operator
-// // --num; // pre-decrement operator
+// var num1 = "3";
+// var num2 = 3;
+// console.log(num1 == num2); // only compare values
+// console.log(num1 === num2); // compare both values and data-types
 
 
-
-
-// // num++; post-increment operator 
-
-// // num = num - 1; // decrement operator
-
-
-// // var message = "Thanks for your input!";
-// // var newMeessage = "Thanks" + " User";
-// // console.log(newMeessage)
-
-// var name = "Asad";
-// alert("Hello " + name + ", Welcome to JavaScript!");
-
-
-
-// var sum = "23" + 7;
-// console.log(sum);
-
-
-// var name = prompt("What is your name?", "John");
-// console.log(name);
-
-// alert("Hello " + name + " Welcome")
-
-var num1 = prompt("Enter num 1?");
-var num2 = prompt("Enter num 2 ?");
-console.log(typeof num1, typeof num2);
-console.log(num1, num2);
-var sum = parseInt(num1) + parseInt(num2);
-alert("The sum is " + sum);
-
-
-
-var num1 = 3;
-var num2 = 4;
-console.log(num1 != num2);
 
 // Comparision Operators
+// === // equality operator
+// !== // not equal operator
 // == // equality operator
 // != // not equal operator
 // < // less than operator
@@ -54,7 +16,86 @@ console.log(num1 != num2);
 // >= // greater than or equal to operator
 
 
+
+
+
 // Logical Operators
 // && // logical AND operator
 // || // logical OR operator
 // ! // logical NOT operator
+
+
+
+// CONTROL STRUCTURES
+// IF STATEMENTS
+
+// var age = 18;
+// if (age >= 18) {
+//     console.log("WELCOME TO ADVENTURE LAND")
+// }
+// var score = 81.1;
+// if (score > 80) {
+//     console.log("Congratulations! You scored:", score)
+// }
+
+// if (test - condition) {
+//     console.log("Congratulations! You scored:", score)
+// }
+
+// If-Else Statements
+var age = 18;
+if (age >= 18) {
+    console.log("welcome")
+}
+else {
+    console.log("you're not allowed")
+}
+
+// if (test - condition) {
+// block of statements
+//     console.log("Congratulations! You scored:", score)
+// }
+// else {
+// else block of statements
+// }
+
+
+
+// If-Else-If OR Else-If Statements
+
+var percentage = 85;
+if (percentage >= 80) {
+    console.log("Grade A-1")
+}
+else if ((percentage >= 70) && (percentage < 80)) {
+    console.log("Grade A")
+}
+else if ((percentage >= 60) && (percentage < 70)) {
+    console.log("Grade B")
+}
+else {
+    console.log("Grade F")
+}
+
+
+// if (test-condition-1) {
+//     console.log("Grade A-1")
+// }
+// else if (test-condition-2) {
+//     console.log("Grade A")
+// }
+// else if (test-condition-3) {
+//     console.log("Grade B")
+// }
+// else {
+//     console.log("Grade F")
+// }
+
+// Challenge 2 (The Thermometer): 
+// Create a variable named temperature.
+// If it is above 30, log "It's hot outside". 
+// If it's between 15 and 30, log "It's pleasant".
+// If it's below 15, log "Brrr, grab a coat!".
+
+
+
