4b19e17b79ed47aa09e3c740518243ae659c87fe
saimmalik.dev <saimalik.dev@gmail.com>
2026-06-17 16:03:31 +0500

    nested loops

--- app.js
+++ app.js
@@ -1,109 +1,68 @@
-// --------    Loops  ----------
-// Loops are used to repeat a block of code multiple times until a certain condition is met. There are different types of loops in JavaScript, including:
 
-// 1. For Loop  (Counter Loop) In this loop, you specify the initialization, test condition, and increment/decrement in a single line. It is commonly used when the number of iterations is known.
-// It is also known as pre-test loop because the condition is evaluated before the execution of the loop body. If the condition is false at the beginning, the loop body will not execute at all.
+// // Car cruise control
+// var speed = 1;
+// var pressAccelarator = 0;
+// while (speed <= 60) {
+//     ++pressAccelarator
+//     console.log("press acccelerator", pressAccelarator);
+//     speed = speed + 1;
+//     console.log("change in speed", speed);
 
-
-// 2. While Loop
-// 3. Do-While Loop
-
-
-
-// console.log(1)
-// console.log(2)
-// console.log(3)
-// console.log(4)
-// console.log(5)
-// console.log(6)
-// console.log(7)
-// console.log(8)
-// console.log(9)
-// console.log(10)
-
-
-
-// 1. For Loop
-// for (initialization, test-condition, incremenet/decrement) {
-//     code to be executed
 // }
 
 
-// for (var i = 10; i >= 1; i--) {
-//     console.log(i)
+// for( pressAccelarator; pressAccelarator){
 // }
 
 
 
 
-// -----------------------------------------------------------------
-// 2. While Loop
-// while (test-condition) {
-//      code to be executed
+
+
+// var machines = 1;
+// for (machines; machines <= 5; ++machines) {
+//     console.log("Machine No# " + machines +  " ON");
 // }
 
-// while (true) {
-//     console.log("While loop is running")
-// }
 
-// var i = 1
-// while (i <= 10) {
-//     console.log(i)
-//     i++
-// }
+// Your Goal: Write a loop that starts a speed variable at 0. As long as the speed is less than 100, add 20 to it and print the current speed. Stop the loop the exact moment it hits 100.
 
-// Real World Use case
-// var password = ""
-// while (password != "secure123") {
-//     password = prompt("Enter your password");
-//     if (password != "secure123") {
-//         alert("Invalid password")
+
+
+
+
+
+// NESTED LOOPS
+
+// for (var i = 1; i <= 10; i++) { // outer loop
+//     console.log(i);
+//     for (var j = 1; j <= 5; j++) { // inner loop
+//         console.log("value of j is", j);
 //     }
 // }
 
-// console.log("Access granted");
+
+// Print SQUARE
+// for (var height = 1; height <= 3; ++height) {
+//     console.log("row 1");
+
+//     // console.log("*".repeat(height))
+//     for (var width = 1; width <= 3; ++width) {
+//         // console.log("col 1");
+
+//         console.log("*".repeat(3))
+//     }
+// }
 
 
+for (var height = 1; height <= 3; ++height) {
+    // console.log("row 1");
 
+    // console.log("*".repeat(height))
+    // for (var width = 1; width <= 3; ++width) {
+        // console.log("col 1");
 
-// -----------------------------------------------------------------
-// Do-While Loop
-// do {
-
-//     //  loop body will be here
-
-// } while (test - condition);
-
-
-// var i = 1;
-// do {
-//     console.log(i);
-//     i++
-// } while (i <= 10);
-
-
-var isFull = "";
-
-do {
-    console.log("You ate a cookie! 🍪");
-
-    isFull = prompt("Are you full? (Type 'yes' to stop)");
-
-} while (isFull != "yes")
-
-console.log("No more cookies for you!");
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+        console.log("*")
+    // }
+}
 
