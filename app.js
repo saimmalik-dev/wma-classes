f4410b4682f798d658fa2d626747a65e981eccec
saimmalik.dev <saimalik.dev@gmail.com>
2026-07-06 16:05:44 +0500

    oop basics in js

--- app.js
+++ app.js
@@ -1,101 +1,131 @@
-// Numbers in JS
-var randomNumber = Math.random();
-console.log(randomNumber);
+// // OOP IN JAVASCRIPT
+// // First we will learn about object and then we will learn about classes and objects in javascript
+
+// var car = {
+//     model: "Corolla",   // key: value
+//     color: "Black",
+//     speed: 200,
+//     engine: "1500cc",
+//     sunRoof: true,
+//     brand: "TOYOTA",
+//     twoSeater: true,
+//     startCar: function (ignition = false) {
+//         if (ignition === true) {
+//             console.log("Start the car");
+//             return ("Car has started");
+//         }
+//         return "First ON the switch";
+
+//     }
+// };
+
+// car.modal = "Civic"
 
 
-var num = 4.100000;
-console.log(Math.ceil(num));
+// var newObj = {} // Object Literal
 
+// console.log(typeof car);
+// console.log("car object", car);
+// console.log("car.color", car.color);
+// console.log("car top speed", car.speed);
+// console.log("car start", car.startCar(true));
 
-// var currentAge = prompt("Enter your age.");
-// console.log(typeof currentAge);
-// var integarVal = parseInt(currentAge)
-// console.log("integarVal", integarVal);
-
-// var floatVal = parseFloat(currentAge)
-// console.log("floatVal", floatVal);
-
-// var incrementedAge = parseInt(currentAge) + 1;
-// console.log(incrementedAge);
-
-
-
-
-// var percentage = prompt("Enter your percentage")
-// var parsedVal = parseFloat(percentage)
-// console.log("parsedVal", parsedVal);
-
-// var prettyPercentage = parsedVal.toFixed(2);
-
-// console.log(prettyPercentage);
-
-
-// FUNCTIONS IN JS
-
-
-var num1 = 3
-var num2 = 5;
-var sum = num1 + num2;
-
-// Some block of code
-
-
-
-var num3 = 45;
-var num4 = 6
-var sum1 = num3 + num4;
+// Using Dot Notation
+// console.log(obj.name);
+// console.log("car.color", car.color);
 
 
 
 
-// Some block of code
+// class CarClass {
 
-var num3 = 45;
-var num4 = 6
-var sum1 = num3 + num4;
+//     constructor(color, brand) {  // A constructor in JavaScript is a special function used to create and initialize object.
+//         this.color = color;
+//         // this.brand = brand;
+//     }
+
+// }
+
+// var carObj1 = new CarClass()
+// console.log(carObj1);
+// var carObj2 = new CarClass("Red", "Audi")
+// console.log(carObj2);
+
+
+class Student {
+    constructor(name, rollNum, age) {
+        this.name = name;
+        this.RollNum = rollNum;
+        this.age = age;
+    }
+
+    getStudentName() {
+        return "Name: " + this.name + " RoLLNUM " + this.RollNum;
+    }
+}
+
+
+var student1 = new Student("Asad", "105", 25)
+var student2 = new Student("Ali", "100", 20)
+
+console.log("student1", student1.name);
+console.log("student2 name", student1.getStudentName());
+console.log("student2", student2);
+console.log("student2 name", student2.getStudentName());
 
 
 
 
-
-
-
-// SYNTAX: OF FUNCTION IN JS
-// function function_name ()  {  
-// body of the function
+// // Without OOP (Procedural Programming)
+// var stdName1 = "Name";
+// var stdRoll1 = "Name";
+// var stdAge1 = 12;
+// function getStudentName1() {
+//     return "Name" + stdName1
 // }
 
 
-// Calling a function
-// function_name();
-
-
-
-
-function add(num1, num2) {
-
-    var sum = num1 + num2;
-
-
-}
-
-
-add(1, 5)
+// var stdName2 = "Name2";
+// var stdRoll2 = "Nam2";
+// var stdAge2 = 12;
+// function getStudentName2() {
+//     return "Name" + stdName2
+// }
 
 
 
 
 
 
+// var personName = "my name";
 
-add(4, 5)
+// const person = {
+//     personName: "GeeksforGeeks",
+//     greet() {
+//         return "Welcome To " + this.personName;
+//     }
+// };
+// console.log(person.greet());
 
 
-function greetUser(name) {
-    alert("Hello " + name)
-}
 
 
-greetUser('Ali')
-greetUser('Asad')
-// greetUser('Saim')
+
+var date = new Date();
+console.log("date", date);
+
+var currYear = date.getFullYear();
+var currMonth = date.getMonth();
+var currMonth = date.toISOString();
+console.log("currMonth", currMonth.split("T")[0]);
+
+var currDate = date.getDate();
+console.log(currDate);
+
+var Now = currDate + currMonth + currYear;
+
+console.log("Now", Now);
+
+// var today = date.get();
+console.log("currYear", currYear);
+
