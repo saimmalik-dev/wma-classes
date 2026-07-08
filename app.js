2185011a77ca0c31d329b2babb0e3aa4a29a6727
saimmalik.dev <saimalik.dev@gmail.com>
2026-07-08 16:02:55 +0500

    oop in js

--- app.js
+++ app.js
@@ -1,131 +1,71 @@
-// // OOP IN JAVASCRIPT
-// // First we will learn about object and then we will learn about classes and objects in javascript
+// OOP IN JAVASCRIPT: Class #2
 
-// var car = {
-//     model: "Corolla",   // key: value
-//     color: "Black",
-//     speed: 200,
-//     engine: "1500cc",
-//     sunRoof: true,
-//     brand: "TOYOTA",
-//     twoSeater: true,
-//     startCar: function (ignition = false) {
-//         if (ignition === true) {
-//             console.log("Start the car");
-//             return ("Car has started");
-//         }
-//         return "First ON the switch";
-
-//     }
-// };
-
-// car.modal = "Civic"
-
-
-// var newObj = {} // Object Literal
-
-// console.log(typeof car);
-// console.log("car object", car);
-// console.log("car.color", car.color);
-// console.log("car top speed", car.speed);
-// console.log("car start", car.startCar(true));
-
-// Using Dot Notation
-// console.log(obj.name);
-// console.log("car.color", car.color);
-
-
-
-
-// class CarClass {
-
-//     constructor(color, brand) {  // A constructor in JavaScript is a special function used to create and initialize object.
-//         this.color = color;
-//         // this.brand = brand;
-//     }
-
-// }
-
-// var carObj1 = new CarClass()
-// console.log(carObj1);
-// var carObj2 = new CarClass("Red", "Audi")
-// console.log(carObj2);
-
-
-class Student {
-    constructor(name, rollNum, age) {
-        this.name = name;
-        this.RollNum = rollNum;
-        this.age = age;
+class Vehicle {
+    constructor(model, color, wheelCount) { // used to create and initialize an object created with a class.
+        this.model = model;
+        this.color = color;
+        this.wheelCount = wheelCount;
     }
 
-    getStudentName() {
-        return "Name: " + this.name + " RoLLNUM " + this.RollNum;
+    // method
+    startVehicle(iginition = false) {
+        if (iginition === false) {
+            console.log("First ON the switch");
+            return;
+        }
+        console.log("Starting the vehicle.....");
+        console.log("Vehicle has started");
     }
 }
 
 
-var student1 = new Student("Asad", "105", 25)
-var student2 = new Student("Ali", "100", 20)
 
-console.log("student1", student1.name);
-console.log("student2 name", student1.getStudentName());
-console.log("student2", student2);
-console.log("student2 name", student2.getStudentName());
-
-
+var car = new Vehicle("Corolla", "Black", 4);
+console.log("car", car)
+var isKeyOn = false;
+car.startVehicle(isKeyOn);
+// var bike = new Vehicle("Honda", "Black", 2);
+// console.log("bike", bike)
 
 
-// // Without OOP (Procedural Programming)
-// var stdName1 = "Name";
-// var stdRoll1 = "Name";
-// var stdAge1 = 12;
-// function getStudentName1() {
-//     return "Name" + stdName1
+
+
+
+
+var person = {
+    // key: value
+    name: "Asad",
+    age: 23,
+    'first-namee': 23,
+    // nested object
+    innerObj: {},
+}
+// console.log(person['first-namee'])
+// console.log(person['age'])
+
+// FOR-in Loop,  used to get object keys
+for (i in person) {
+    console.log("keys", i);
+}
+
+
+var carObj = Object.keys(car)
+var carObjVal = Object.values(car)
+console.log(carObj);
+console.log(carObjVal);
+
+
+
+// var array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// for (var value of array) {
+//     console.log("value", value);
 // }
 
 
-// var stdName2 = "Name2";
-// var stdRoll2 = "Nam2";
-// var stdAge2 = 12;
-// function getStudentName2() {
-//     return "Name" + stdName2
+// for (var i = 0; i <= array.length; i++) {
+//     console.log("value", array[i]);
 // }
 
 
 
-
-
-
-// var personName = "my name";
-
-// const person = {
-//     personName: "GeeksforGeeks",
-//     greet() {
-//         return "Welcome To " + this.personName;
-//     }
-// };
-// console.log(person.greet());
-
-
-
-
-
-var date = new Date();
-console.log("date", date);
-
-var currYear = date.getFullYear();
-var currMonth = date.getMonth();
-var currMonth = date.toISOString();
-console.log("currMonth", currMonth.split("T")[0]);
-
-var currDate = date.getDate();
-console.log(currDate);
-
-var Now = currDate + currMonth + currYear;
-
-console.log("Now", Now);
-
-// var today = date.get();
-console.log("currYear", currYear);
 
