2d8d78d49aa01f33a195fdf3702ec35d12c77d6c
saimmalik.dev <saimalik.dev@gmail.com>
2026-08-21 16:08:48 +0500

    arrow vs regular fn, this keyword

--- app.js
+++ app.js
@@ -1,103 +1,164 @@
-// // Scopes, Lexical Scope, Scope Chaining, TDZ and hoisting.
-
-// Global vs Local Scope
-// // let usernAME = "ALI" // global scope
-// if (4 < 5) {
-//    var usernAME = "ALI" // local scope
-//     console.log(usernAME);
-
-// }
-// console.log(usernAME);
+// // Lexical Scope, this keyword, Regular and Arrow Functions, Desturcturing, Rest & Spread operator
 
 
-// // Lexical Scope 
+// // // Lexical Scope 
 // // Lexical scope means a function can access variables from the scope in which it was defined. The scope is determined by the code structure, not by where the function is called.
 
-// function outer() {
-//     let userName = "Ahmed";
-//     function inner() {
-//         // let innervar = "no name"
-//         console.log(" innner function:", userName);
-//     }
+// // Lexical scope means that variable accessibility is determined entirely by the physical position of the code at compile time, not where the function is executed.
+
+// // lexical scope mein kahan likha hai yeh matter krta hai. na k yeh k kahan pr call/execute hua.
+
+// // function outer() {
+
+// //   let userName = "Ahmed";
+// //   function inner() {
+// //     // let innervar = "no name"
+// //     console.log(" innner function:", userName);
+// //   }
+
+// //   inner()
+// //   console.log("outer function:", userName);
+// // }
+
+// // outer();
+
+// //  ------------- Lexical Scope Example -------------------
+// // let globalVar = "I am a global variable";
+
+// // function outerFunction() {
+// //   let outerVar = "I am an outer variable";
+
+// //   function innerFunction() {
+// //     let innerVar = "I am an inner variable";
+// //     console.log(globalVar); // Found in the global scope
+// //     console.log(outerVar); // Found in the outer function scope
+// //     console.log(innerVar); // Found in the inner function scope
+// //   }
+
+// //   innerFunction();
+// // }
+
+// // outerFunction();
 
 
-//     inner()
-//     console.log("outer function:", userName);
-// }
 
-// outer();
-// 
+// // This keyword.
+// // Method invokation: this is determined by the owner object of that method.
 
-//  ------------- Lexical Scope Example -------------------
-// let globalVar = "I am a global variable";
+// const obj = {
+//   userName: "ali",
+//   // test: "ali",
+//   greet: function () {
+//     console.log("this", this);
+//     console.log('Hello: ' + this.userName);
 
-// function outerFunction() {
-//   let outerVar = "I am an outer variable";
+//     let user = "user";
+//     console.log("user", user);
 
-//   function innerFunction() {
-//     let innerVar = "I am an inner variable";
-//     console.log(globalVar); // Found in the global scope
-//     console.log(outerVar); // Found in the outer function scope
-//     console.log(innerVar); // Found in the inner function scope
+//   },
+//   arrowGreet: () => {
+//     console.log("Arrow fn",this);
+
+//     console.log(`Hello: ${this}`);
+
+//     // let user = "user";
+//     // console.log("user", user);
+
 //   }
+// }
+// obj.greet()
+// obj.arrowGreet()
 
-//   innerFunction();
+
+// // let name = "ali"
+// // alert("Hello " + name)
+
+
+
+
+// // var name = "asad"
+
+// // function userDetails() {
+
+// //   console.log(this);
+// //   console.log(this.name);
+// //   console.log("Here is a user details " + this.name);
+
+
+// // }
+
+// // userDetails()
+
+
+// // function fnName(a, b) {
+// //   return a + b
+// // }
+// // fnName(3, 4)
+
+// // const arrowFn = (a, b) => {
+// //   return a + b
+// // };
+// // console.log(arrowFn(3, 4))
+
+
+
+
+// // Function declaration vs Function expression
+// // Function Declaration
+// function fnName(a, b) {
+//   return a + b
+// }
+// fnName(3, 4)
+// // fnName function is not assigned to any variable;
+
+// // Function EXPRESSION
+// // if function declaration start with variable idenfier then it would be function expression.
+// const fnExpression = function (a, b) {
+//   return a + b
+// }
+// fnExpression(3, 4)
+
+
+
+// // Arrow Function: 
+// // arrow functions are always in expression
+// const arrowFn = () => {
+
 // }
 
-// outerFunction();
+
+
+// greet(); // ReferenceError: Cannot access 'greet' before initialization
+
+// const greet = () => {
+//     console.log('Hello!');
+// };
 
 
 
 
 
 
-// // Hoisting: the process or mechanism of moving all variable, function, or class declarations to the top of the scope or "js file if global". It moves top conceptually not really.
-// console.log(x);
 
-// x = 5; // Assign 5 to x
-// var x; // Declare x
-// console.log(x);
+// const obj = {
+//   objName: "ali",
+//   greet: function () {
+//     // console.log("this", this);
+//     console.log('Hello: ' + this.objName);
+//   },
+//   arrowGreet: () => {
+//     // console.log("Arrow fn",this);
 
-// // Hoisting function:
-// hoisting();
+//     console.log(`Hello: ${this.objName}`);
 
-// function hoisting() {
-//     console.log("THIS IS FUNCTION");
-
+//   }
 // }
-
-
-// console.log(y);
-
-// y = 10; // Assign 10 to y
-// var y; // Declare y
-// console.log(y);
-
-
-// Temporal Dead Zone (TDZ)
-// TDZ a area where variable declared with let/const in placed temporary before initialization 
-// console.log(y);
-
-y = 10; // Assign 10 to y
-let y; // Declare y
-console.log(y);
-
-
-// console.log(username);
-// var username = "ali"
-
-// console.log(username);
+// obj.greet()
+// obj.arrowGreet()
 
 
 
-// console.log(letusername);
-// let letusername = "ali"
 
-// console.log(letusername);
+// [1, 2, 3, 4, 5, 6] -> [4, 5, 6] -> [5, 6] -> 6
 
-function demoTDZ() {
-  console.log(myLet); // ReferenceError
-  let myLet = 100;
-  console.log(myLet); // 100
-}
-demoTDZ();
+// ['ali', 'asad','amir','hassan']
